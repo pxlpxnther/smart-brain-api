cab660e49009ae8100b5ef7492906372fe4ce6e8
Jordan Myers <jordanmyers@Jordans-MBP.hitronhub.home>
2020-03-19 16:54:24 -0600

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const clarifai = require('clarifai');
+const app = new Clarifai.App({
+  apiKey: 'de78f68233e84c0eae6899e57da81442',
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'));
+};
+
+module.exports = {
+  handleImage,
+  handleApiCall,
+};
